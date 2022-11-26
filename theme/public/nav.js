d6c66a855f8474fa3fe4756426245dee093156db
Timur <tima@nyu.edu>
2022-11-26 18:55:13 -0500

    wedding gallery

---
+++ theme/public/nav.js
@@ -0,0 +1,114 @@
+(function($) {
+
+	"use strict";
+
+
+    /*------------------------------------------
+        = FUNCTIONS
+    -------------------------------------------*/
+    // Check ie and version
+    function isIE () {
+        var myNav = navigator.userAgent.toLowerCase();
+        return (myNav.indexOf('msie') != -1) ? parseInt(myNav.split('msie')[1], 10) : false;
+    }
+
+
+    // Toggle mobile navigation
+    function toggleMobileNavigation() {
+        var navbar = $(".navigation-holder");
+        var openBtn = $(".navbar-header .open-btn");
+        var closeBtn = $(".navigation-holder .close-navbar");
+        var navLinks = $("#navbar > ul > li > a[href^='#']");
+
+        openBtn.on("click", function() {
+            if (!navbar.hasClass("slideInn")) {
+                navbar.addClass("slideInn");
+            }
+            return false;
+        })
+
+        closeBtn.on("click", function() {
+            if (navbar.hasClass("slideInn")) {
+                navbar.removeClass("slideInn");
+            }
+            return false;
+        })
+
+        navLinks.on("click", function() {
+            if (navbar.hasClass("slideInn")) {
+                navbar.removeClass("slideInn");
+            }
+            return false;
+        })
+    }
+
+    toggleMobileNavigation();
+
+
+    // Function for toggle a class for small menu
+    function toggleClassForSmallNav() {
+        var windowWidth = window.innerWidth;
+        var mainNav = $("#navbar > ul");
+
+        if (windowWidth <= 991) {
+            mainNav.addClass("small-nav");
+        } else {
+            mainNav.removeClass("small-nav");
+        }
+    }
+
+    toggleClassForSmallNav();
+
+
+    // Function for small menu
+    function smallNavFunctionality() {
+        var windowWidth = window.innerWidth;
+        var mainNav = $(".navigation-holder");
+        var smallNav = $(".navigation-holder > .small-nav");
+        var subMenu = smallNav.find(".sub-menu");
+        var megamenu = smallNav.find(".mega-menu");
+        var menuItemWidthSubMenu = smallNav.find(".menu-item-has-children > a");
+
+        if (windowWidth <= 991) {
+            subMenu.hide();
+            megamenu.hide();
+            menuItemWidthSubMenu.on("click", function(e) {
+                var $this = $(this);
+                $this.siblings().slideToggle();
+                 e.preventDefault();
+                e.stopImmediatePropagation();
+            })
+        } else if (windowWidth > 991) {
+            mainNav.find(".sub-menu").show();
+            mainNav.find(".mega-menu").show();
+        }
+    }
+
+    smallNavFunctionality();
+
+
+    // function for active menuitem
+    function activeMenuItem($links) {
+        var top = $(window).scrollTop(),
+            windowHeight = $(window).height(),
+            documentHeight = $(document).height(),
+            cur_pos = top + 2,
+            sections = $("section"),
+            nav = $links,
+            nav_height = nav.outerHeight();
+
+
+        sections.each(function() {
+            var top = $(this).offset().top - nav_height,
+                bottom = top + $(this).outerHeight();
+
+            if (cur_pos >= top && cur_pos <= bottom) {
+                nav.find("> ul > li > a").parent().removeClass("current-menu-item");
+                nav.find("a[href='#" + $(this).attr('id') + "']").parent().addClass("current-menu-item");
+            } else if (cur_pos === 2) {
+                nav.find("> ul > li > a").parent().removeClass("current-menu-item");
+            }
+
+        });
+    }
+})(window.jQuery);
